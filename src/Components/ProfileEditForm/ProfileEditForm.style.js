995810802f086482a4ce12cf7aaa72c8f4b016a0
kashif mughal <123m.kashifmughal@gmail.com>
2021-07-16 18:40:55 +0500

    friday work

--- src/Components/ProfileEditForm/ProfileEditForm.style.js
+++ src/Components/ProfileEditForm/ProfileEditForm.style.js
@@ -1,17 +1,28 @@
 export const style = ()=> ({
   inputClass : {
     fontSize : "1.5rem",
-    lineHeight : "2rem",
+    lineheight : "3rem"
+  },
+  lineHeight : {
+    fontSize : "1.4rem",
+    lineHeight : "1.7rem",
     boxSizing : "border-box"
   },
   marginClass : {
-    margin : "1rem 0"
+    marginBottom : "1rem",
   },
   marginClass2 : {
     margin : "1rem 0",
-    marginBottom : "3rem"
+    marginBottom : "3rem",
+    boxSizing : 'border-box'
   },
   marginClass3 : {
     margin : "1rem 0",
+  },
+  marginbottom : {
+    marginBottom : "2rem",
+  },
+  margintop : {
+    marginTop : "2rem",
   }
 });
