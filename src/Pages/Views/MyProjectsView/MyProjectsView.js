3f686491627a29fdf1f5a455086e65526fb7f9a0
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2021-07-27 18:50:13 +0500

    my project view updated

--- src/Pages/Views/MyProjectsView/MyProjectsView.js
+++ src/Pages/Views/MyProjectsView/MyProjectsView.js
@@ -2,8 +2,9 @@ import React,{useEffect} from 'react'
 import './MyProjectsView.css'
 import { connect } from 'react-redux';
 import { GetMyProjects } from '../../../Redux/PostedProjects/PostedProjectsAction';
+import PostCard from '../../../Components/PostCard/PostCard';
 
-var MyProjectsView = ({myProjects,GetMyProjects})=> {
+var MyProjectsView = ({myProjects, GetMyProjects})=> {
     useEffect(() => {
         GetMyProjects();        
     }, [])
@@ -11,8 +12,9 @@ var MyProjectsView = ({myProjects,GetMyProjects})=> {
     console.log(myProjects)
 
     return (
-        <div>
-            
+        <div className='my-project-view-container'>
+            <h1 className='my-project-view-heading'>My Projects</h1>
+            {myProjects.map( project => <PostCard key={project.id} project={project} editable={true} />)}
         </div>
     )
 }
