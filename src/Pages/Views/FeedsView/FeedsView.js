19535250a57b33958ad342e91ce593c16ed17239
kashif mughal <123m.kashifmughal@gmail.com>
2021-07-27 16:44:23 +0500

    a

--- src/Pages/Views/FeedsView/FeedsView.js
+++ src/Pages/Views/FeedsView/FeedsView.js
@@ -1,30 +1,33 @@
-import React,{ useEffect } from 'react'
-import PostCard from '../../../Components/PostCard/PostCard'
-import './FeedsView.css'
-import { GetAllProjects } from './../../../Redux/Projects/ProjectsActions';
-import { connect } from 'react-redux';
+import React, { useEffect } from "react";
+import PostCard from "../../../Components/PostCard/PostCard";
+import "./FeedsView.css";
+import { GetAllProjects } from "./../../../Redux/Projects/ProjectsActions";
+import { connect } from "react-redux";
 
-var FeedsView = ({GetAllProjects,projects})=> {
-    useEffect(() => {
-        GetAllProjects();
-    }, [])
+var FeedsView = ({ GetAllProjects, projects }) => {
+  useEffect(() => {
+    GetAllProjects();
+  }, []);
 
-    return (
-        <div className="feeds-view">
-            <PostCard />
-            <PostCard />
-            <PostCard />
-            <PostCard />
-        </div>
-    )
-}
+  return (
+    <div className="feeds-view">
+        {console.log(projects)}
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
+    </div>
+  );
+};
 
 var actions = {
-    GetAllProjects
-}
+  GetAllProjects,
+};
 
 const mapState = (state) => {
-    projects : state.Projects
-}
+  return {
+    projects: state.Projects,
+  };
+};
 
-export default connect(mapState,actions)(FeedsView)
+export default connect(mapState, actions)(FeedsView);
