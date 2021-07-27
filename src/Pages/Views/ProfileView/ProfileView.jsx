74112a57d7bee7406e0b6be3e4a83523fd2366a7
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2021-07-27 12:52:31 +0500

    profile view altered

--- src/Pages/Views/ProfileView/ProfileView.jsx
+++ src/Pages/Views/ProfileView/ProfileView.jsx
@@ -1,45 +1,53 @@
-import React from 'react'
-import Chips from './../../../Components/Chips/Chips';
-import PostCard from './../../../Components/PostCard/PostCard';
-import Page2 from './ProfileView2';
+import React, { useEffect } from 'react'
+import AOS from 'aos';
+import "aos/dist/aos.css";
+import PostCard from '../../../Components/PostCard/PostCard';
 import {connect} from 'react-redux'
 import { withStyles } from '@material-ui/core';
 import { useStyles } from './profileView.style';
 
 var ProfileView = ({profile, classes})=> {
-    console.log(profile);
-    const {id, name, email, picture, dob, description, contact, keywords} = profile
+    const {id, name, email, picture, dob, description, contact, keywords} = profile;
+    useEffect(() => {
+        AOS.init({
+          duration : 1000
+        });
+      }, []);
     return (
         <div className={classes.container}>
-            <div className={classes.basic}>
-                <div className={classes.avatar}>
+            <div className={classes.leftBar}>
+                <div data-aos="flip-left" className={classes.avatar} >
                     <img src={picture} alt="Profile Image" />
                 </div>
-                <div className={classes.details}>
-                    <div className={classes.name}><b>Name: </b>{name}</div>
-                    <div className={classes.email}>{email}</div>
-                    <div className={classes.contact}><b>Contact: </b>{contact}</div>
-                    <div className={classes.dob}><b>Date of Birth: </b>{dob}</div>
-                    <div className={classes.description}>
-                        <b>Description: </b>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium ullam eos nulla beatae, tempora necessitatibus distinctio alias esse porro consectetur corrupti quia sapiente earum perferendis aliquam doloribus sunt, itaque voluptatum.
-                        </p>
-                    </div>
-                    <div className={classes.domain}>
-                        <b>Domains: </b>
-                        <div className={classes.projectDomain}>
-                            {keywords.map((name)=><Chips name={name} />)}
-                        </div>
+                <div data-aos="fade-right" className={classes.domain} >
+                    <p className={classes.domainHeading}><b>Domain</b></p>
+                    <div className={classes.domainList}>
+                        {keywords.map((feild, index)=>
+                            <p data-aos="fade-right" data-aos-anchor-placement="top-bottom" data-aos-duration={index*150+1200} className={classes.domainItem}>{feild.charAt(0).toUpperCase() + feild.slice(1)}</p>
+                        )}
                     </div>
                 </div>
             </div>
-            <div className={classes.myProjects}>
-                <PostCard />
-                <PostCard />
-                <PostCard />
-                <PostCard />
-                <Page2 />
+            <div className={classes.rightBar} >
+                <div data-aos="fade-left" className={classes.details}>
+                    <p className={classes.name}><b>{name}</b></p>
+                    <p className={classes.email}>{email}</p>
+                    <p className={classes.contact}><i class="fas fa-phone"></i>{contact}</p>
+                    <p className={classes.dob}><i class="far fa-calendar-alt"></i>{dob}</p>
+                </div>
+                <div data-aos="zoom-in" data-aos-duration="1500" className={classes.about}>
+                    <h1>About</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium ullam eos nulla beatae, tempora necessitatibus distinctio alias esse porro consectetur corrupti quia sapiente earum perferendis aliquam doloribus sunt, itaque voluptatum.
+                    </p>
+                </div>
+                <div className={classes.projects}>
+                    <h1 data-aos="fade-left" data-aos-anchor-placement="top-bottom" className={classes.projectHeading}>MY PROJECTS</h1>
+                    <div data-aos="fade-left" data-aos-anchor-placement="top-bottom" ><PostCard /></div>
+                    <div data-aos="fade-left" data-aos-anchor-placement="top-bottom" ><PostCard /></div>
+                    <div data-aos="fade-left" data-aos-anchor-placement="top-bottom" ><PostCard /></div>
+                    <div data-aos="fade-left" data-aos-anchor-placement="top-bottom" ><PostCard /></div>
+                </div>
             </div>
         </div>
     )
