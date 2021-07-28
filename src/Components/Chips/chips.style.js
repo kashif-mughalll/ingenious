a9f5456d63624413ecdd0a382e1037bcda38b62b
kashif mughal <123m.kashifmughal@gmail.com>
2021-07-28 19:13:14 +0500

    styles edited

--- src/Components/Chips/chips.style.js
+++ src/Components/Chips/chips.style.js
@@ -5,10 +5,12 @@ export const useStyles = () => ({
     margin: "0.6rem",
   },
   simple: {
-    margin: "0.6rem",
+    margin: "0.7rem",
+    marginLeft: "0rem",
   },
   label: {
-    fontSize: "1.2rem",
+    fontSize: "1.3rem",
+    padding : '1.2rem'
   },
   focus: {
       '&:focus': {
