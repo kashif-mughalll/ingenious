e5bebd73b71c70f2103aa49dafd0674b2843013d
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2021-07-28 19:14:11 +0500

    bug from chips hover effect removed

--- src/Components/Chips/Chips.jsx
+++ src/Components/Chips/Chips.jsx
@@ -2,21 +2,18 @@ import { withStyles } from "@material-ui/core";
 import Chip from "@material-ui/core/Chip";
 import { useStyles } from "./chips.style";
 
-const Chips = ({ classes, name, checked=false, handleChange=null }) => {
+const Chips = ({ classes, name, checked = false, handleChange = null }) => {
   return (
     <Chip
       label={name}
-      classes={{
-        clickable	: {
-          '&:focus': {
-            backgroungColor: 'black'
-          }
-        }
-      }}
+      // classes={{
+      //   clickable	: 'ahad'
+      // }}
       size="small"
       className={checked ? classes.checked : classes.simple}
       classes={{
         labelSmall: classes.label,
+        clickable: classes.focus,
       }}
       onClick={handleChange}
     />
