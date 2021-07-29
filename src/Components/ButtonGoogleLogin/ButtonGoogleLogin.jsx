68a32e95eee10416e873810568855dfb98c75107
kashif mughal <123m.kashifmughal@gmail.com>
2021-07-29 16:10:06 +0500

    loader work done

--- src/Components/ButtonGoogleLogin/ButtonGoogleLogin.jsx
+++ src/Components/ButtonGoogleLogin/ButtonGoogleLogin.jsx
@@ -1,25 +1,37 @@
-import { Button, Icon, withStyles } from "@material-ui/core"
+import { Button, Icon, withStyles } from "@material-ui/core";
 import { useStyles } from "./ButtonGoogle.style";
-import { connect } from 'react-redux';
-import { SignInWithGoogle } from './../../Redux/Auth/authActions';
+import { connect } from "react-redux";
+import { SignInWithGoogle } from "./../../Redux/Auth/authActions";
+import { HideLoader, ShowLoader } from './../../Redux/Loader/LoaderActions';
 
-const ButtonGoogleLogin = ({classes,SignInWithGoogle}) => {
-    return(
-        <Button
-          onClick={SignInWithGoogle}
-          variant="outlined"
-          color="primary"
-          fullWidth 
-          className={classes.googleButton}
-          startIcon={<i className="fab fa-google-plus-g" style={{color: 'rgb(160, 76, 76)'}}></i>}
-        >
-          Login With Google
-        </Button>
-    )
-}
+const ButtonGoogleLogin = ({ classes, SignInWithGoogle,ShowLoader,HideLoader }) => {
+  return (
+    <Button
+      onClick={async () => {
+        ShowLoader();
+        SignInWithGoogle();
+        HideLoader();
+      }}
+      variant="outlined"
+      color="primary"
+      fullWidth
+      className={classes.googleButton}
+      startIcon={
+        <i
+          className="fab fa-google-plus-g"
+          style={{ color: "rgb(160, 76, 76)" }}
+        ></i>
+      }
+    >
+      Login With Google
+    </Button>
+  );
+};
 
 const actions = {
-  SignInWithGoogle
-}
+  SignInWithGoogle,
+  ShowLoader,
+  HideLoader
+};
 
-export default connect(null,actions)(withStyles(useStyles)(ButtonGoogleLogin));
+export default connect(null, actions)(withStyles(useStyles)(ButtonGoogleLogin));
