6096d726d55ae4ba300825dde964f51dccd6ad7a
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2021-07-30 13:32:48 +0500

    static keywords replaced with keyeords from database

--- src/Components/CollaborationCard/CollaborationCard.jsx
+++ src/Components/CollaborationCard/CollaborationCard.jsx
@@ -15,21 +15,13 @@ const CollaborationCard = ({
   keywords,
   title
 }) => {
-  function titleCase(str) {
-    var splitStr = str.toLowerCase().split(" ");
-    for (var i = 0; i < splitStr.length; i++) {
-      splitStr[i] =
-        splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
-    }
-    return splitStr.join(" ");
-  }
   return (
     <div className={classes.container}>
       <div className={classes.avatar}>
         <img src={picture} alt="profile picture" />
       </div>
       <div className={classes.details}>
-        You have collaborated in <b>{titleCase(name)}'s</b> project <b>"{title}"</b> as <b>{keywords.join(", ")}</b>
+        You have collaborated in <b>{name}'s</b> project <b>"{title}"</b> as <b>{keywords.join(", ")}</b>
       </div>
       <div className={classes.buttons}>
         <button
--- src/Components/NotificationCard/NotificationCard.jsx
+++ src/Components/NotificationCard/NotificationCard.jsx
@@ -21,21 +21,13 @@ const NotificationCard = ({
   ShowLoader,
   HideLoader
 }) => {
-  function titleCase(str) {
-    var splitStr = str.toLowerCase().split(" ");
-    for (var i = 0; i < splitStr.length; i++) {
-      splitStr[i] =
-        splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
-    }
-    return splitStr.join(" ");
-  }
   return (
     <div className={classes.container}>
       <div className={classes.avatar}>
         <img src={info.picture} alt="profile picture" />
       </div>
       <div className={classes.details}>
-        <b>{titleCase(info.name)}</b> has requested you to collaborate in your
+        <b>{info.name}</b> has requested you to collaborate in your
         project <b>"{title}"</b> as <b>{keywords.join(", ")}</b>
       </div>
       <div className={classes.buttons}>
